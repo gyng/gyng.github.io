59692ed5dfeb150087bda11637c3db267ffaa245
Ng Guoyou <ng.guoyou@gmail.com>
2018-01-03 17:04:02 +0800

    Update projects list, remove skeleton, use CSS grid

--- generate.rb
+++ generate.rb
@@ -6,42 +6,37 @@ image_directory = "images/"
 projects = JSON.parse(File.read('manifest.json'), symbolize_names: true)
 
 project_template = '
-  <section class="one-third column project {{alpha_omega_modifier}}">
+  <div class="project">
     <a href="{{title_link}}">
       <img class="project-image" src="{{image}}">
-      <h4>{{title}}</h4>
+      <div class="project-header">
+        <h3 class="project-title">{{title}}</h3>
+        <div class="project-tag">{{tag}}</div>
+      </div>
     </a>
-    {{subtitles_html}}
-    {{description_html}}
-  </section>'
+    <div class="project-subtitle">
+      <div class="project-links">
+        {{subtitles_html}}
+      </div>
+    </div>
+    <div class="project-description">
+      {{description_html}}
+    </div>
+  </div>'
 
 subtitle_template = '
-  <a href="{{link}}">{{title}}</a>
+  <a class="project-link" href="{{link}}">{{title}}</a>
 '
 
-row_closed = false
-
 File.open('./preindex.html', 'r') do |f|
-  projects_html = ""
+  projects_html = ''
 
-  projects.each_with_index do |project, i|
+  projects.each do |project|
     template = project_template.dup
-
-    # Required for old and outdated skeleton miniframework
-    case i % 3
-    when 0
-      template = "<div class='row'>#{template}"
-      template.gsub!("{{alpha_omega_modifier}}", "alpha")
-      row_closed = false
-    when 2
-      template.gsub!("{{alpha_omega_modifier}}", "omega")
-      template = "#{template}</div>"
-      row_closed = true
-    end
 
     template.gsub!("{{image}}", "#{image_directory}#{project[:image]}")
     template.gsub!("{{title}}", project[:title][:name])
-    template.gsub!("{{title_link}}", project[:title][:link])
+    template.gsub!("{{title_link}}", project[:title][:link] || "")
 
     template.gsub!("{{subtitles_html}}",
       project[:subtitles].map { |s|
@@ -51,12 +46,12 @@ File.open('./preindex.html', 'r') do |f|
       }.join("&nbsp;&middot;&nbsp;")
     )
 
+    template.gsub!("{{tag}}", project[:tag] || '')
+
     template.gsub!("{{description_html}}", project[:description_html])
 
     projects_html = projects_html + template
   end
-
-  projects_html = projects_html + "</div>" if !row_closed
 
   File.write('index.html', f.read.gsub("{{projects}}", projects_html))
 end
